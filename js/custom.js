38a19cd8a2ae1f2ef12486f2b9dd8118b98a86e1
yuliamurziak <murzia4ka@gmail.com>
2018-03-24 14:37:42 -0400

    change2

--- js/custom.js
+++ js/custom.js
@@ -72,3 +72,69 @@ $(document).ready( function(e) {
 
 
 });
+
+/* Julia's work: */
+function CloseWindow(){
+ document.getElementById("overlay").style.display = "none";
+}
+
+function showManageProduct(){
+		var dropdownlist = document.getElementById("manageProduct");
+	manageProduct.style.display="inline";
+}
+function showManageProduct2(){
+		var dropdownlist = document.getElementById("manageProduct2");
+	manageProduct2.style.display="inline";
+}
+function showManageProduct3(){
+		var dropdownlist = document.getElementById("manageProduct3");
+	manageProduct3.style.display="inline";
+}
+
+function showCreateProduct(){
+	
+	 document.getElementById("overlay").style.display = "block";
+}
+
+
+/* function or drag and drop */
+
+var imageLoader = document.getElementById('filePhoto');
+    imageLoader.addEventListener('change', handleImage, false);
+
+function handleImage(e) {
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        $('#uploader img').attr('src',event.target.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+    
+}
+
+var dropbox;
+dropbox = document.getElementById("uploader");
+dropbox.addEventListener("dragenter", dragenter, false);
+dropbox.addEventListener("dragover", dragover, false);
+dropbox.addEventListener("drop", drop, false);
+
+function dragenter(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function dragover(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function drop(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  //you can check e's properties
+  //console.log(e);
+  var dt = e.dataTransfer();
+  var files = dt.files;
+  
+  //this code line fires your 'handleImage' function (imageLoader change event)
+  imageLoader.files = files;
+}
